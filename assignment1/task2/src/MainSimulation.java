7f11d0b3bcc6ca1959abac34064bfbd02dd82c47
Louise Söderström <louise.matematik@gmail.com>
2014-04-18 13:07:45 +0200

    taks2 almost done

--- assignment1/task2/src/MainSimulation.java
+++ assignment1/task2/src/MainSimulation.java
@@ -7,20 +7,20 @@ public class MainSimulation extends GlobalSimulation{
     	Event actEvent;
     	State actState = new State(); // The state that should be used
     	// Some events must be put in the event list at the beginning
-        insertEvent(ARRIVAL, 0);  
-        insertEvent(MEASURE, 5);
+        insertEvent(ARRIVALA, 0);  
+        insertEvent(MEASURE, 0.1);
         
         // The main simulation loop
-    	while (actState.nbrMeasurements < 10000){//1000 enough
+    	while (actState.nbrSamples < 2000){//1000 enough
     		actEvent = eventList.fetchEvent();
     		time = actEvent.eventTime;
     		actState.treatEvent(actEvent);
     	}
     	
     	// Printing the result of the simulation, in this case a mean value
-    	System.out.print("Mean number of customers in Q2: ");
-    	System.out.println(1.0*actState.totNbrInQ2/actState.nbrMeasurements);
-    	System.out.print("Probability of rejection: ");
-    	System.out.println(1.0*actState.nbrRejected/actState.nbrCustomers);
+    	System.out.print("Mean number of customers in buffer: ");
+    	System.out.println(actState.totNbrJobs + "/" + actState.nbrSamples);
+    	System.out.println(1.0*actState.totNbrJobs/actState.nbrSamples);
+    	System.out.println("A: " + actState.testerA + " B: " + actState.testerB);
     }
 }
