76053a31b7a33c753a062a81151fa7597f5e7553
Louise Söderström <louise.matematik@gmail.com>
2014-05-06 13:09:36 +0200

    new structure

---
+++ assignment1/src/task1/MainSimulation.java
@@ -0,0 +1,28 @@
+package task1;
+
+import java.io.*;
+
+
+public class MainSimulation extends GlobalSimulation{
+ 
+    public static void main(String[] args) throws IOException {
+    	Event actEvent;
+    	State actState = new State(); // The state that should be used
+    	// Some events must be put in the event list at the beginning
+        insertEvent(ARRIVAL, 0);  
+        insertEvent(MEASURE, 5);
+        
+        // The main simulation loop
+    	while (actState.nbrMeasurements < 5000){//1000 enough
+    		actEvent = eventList.fetchEvent();
+    		time = actEvent.eventTime;
+    		actState.treatEvent(actEvent);
+    	}
+    	
+    	// Printing the result of the simulation, in this case a mean value
+    	System.out.print("Mean number of customers in Q2: ");
+    	System.out.println(1.0*actState.totNbrInQ2/actState.nbrMeasurements);
+    	System.out.print("Probability of rejection: ");
+    	System.out.println(1.0*actState.nbrRejected/actState.nbrCustomers);
+    }
+}
